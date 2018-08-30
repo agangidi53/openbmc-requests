2d4cd66835bbc0b92442c5a5845cab4fb8b8a948
agangidi53 <root@mwq0jkg8wl.rackspace.corp>
2018-08-30 16:46:46 -0500

    added comments describing the purpose of the repo / files

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # openbmc-requests
-
 Sample test OpenBMC REST API via requests API
+
 To execute , customize test.py to point to your OpenBMC IP / credentials and execute 'test.py':
 
 python test.py
--- test.py
+++ test.py
@@ -1,11 +1,14 @@
-import login
+#Example OpenBMC REST API consumption script
+#Import the openbmc Login class from openbmc.py to enable support for getting sensor data and host manipulation via BMC
+
+import openbmc    # Import openbmc class structure
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 
 username='root'
 password='0penBmc'
-box3=  login.Login('10.127.91.7',username,password)
+box3=  openbmc.Login('10.127.91.7',username,password)
 box3._login()
 box3._enumerate()
 
@@ -27,10 +30,10 @@ for i in range(100):
     plt.scatter(i, box3._gettemp('ambient_mb')/1000, c='red' , label='Ambient temperature')
     plt.scatter(i, box3._gettemp('p0_core0_temp')/1000, c='blue' ,label = 'CPU0 Temperature')
     plt.scatter(i, box3._gettemp('p1_core0_temp')/1000, c='black' ,label = 'CPU1 Temperature')
-#    plt.scatter(i, box3._getspeed('fan0'), c='green' ,label= 'Fan Speed')
+    plt.scatter(i, box3._getspeed('fan0'), c='green' ,label= 'Fan Speed')
     plt.scatter(i, box3._gettemp('dimm0_temp')/1000, c='cyan' , label='DIMM temperature')
     plt.scatter(i, box3._gettemp('dimm31_temp')/1000, c='magenta' , label='DIMM temperature')
-#    plt.scatter(i, box3._gettemp('dimm0_temp')/1000, c='cyan' , label='DIMM temperature')
-#    plt.scatter(i, box3._gettemp('dimm31_temp')/1000, c='cyan' , label='DIMM temperature')
+    plt.scatter(i, box3._gettemp('dimm0_temp')/1000, c='cyan' , label='DIMM temperature')
+    plt.scatter(i, box3._gettemp('dimm31_temp')/1000, c='cyan' , label='DIMM temperature')
     plt.pause(0.05)
 plt.show()
